be37e73fb188cddf3f448021c7a1623812e17900
koralowiec <arek0906@protonmail.com>
2021-05-09 14:23:12 +0200

    Creating a ec2 instance with public key and security group (which allows SSH connection)

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "amazon_linux" {
   most_recent = true
-  owners = ["amazon"]
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
@@ -8,11 +8,40 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
+resource "aws_key_pair" "pubkey" {
+  key_name   = "public-key-tf"
+  public_key = ""
+}
+
+resource "aws_security_group" "ssh-sg" {
+  name = "ssh-tf-sg"
+
+  ingress {
+    description = "SSH connection"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
 resource "aws_instance" "vm" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
+  key_name      = aws_key_pair.pubkey.key_name
 
-  tags =  {
+  tags = {
     Name = "Społeczeństwo-informacyne"
   }
 }
